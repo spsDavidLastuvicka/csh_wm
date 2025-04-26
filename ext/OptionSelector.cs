04bc4ab3a15db9086777db1d132e7cee7d178550
spsDavidLastuvicka <lastuvickadavid@sps-karvina.cz>
2025-04-26 11:28:52 +0200

    Autoformatted OptionSelector.cs

--- ext/OptionSelector.cs
+++ ext/OptionSelector.cs
@@ -1,53 +1,53 @@
 public partial class Tools
 {
     public static int OptionSelector(string Text, params string[] Options)
-	{
-		Console.Clear();
-		Console.WriteLine(Text);
+    {
+        Console.Clear();
+        Console.WriteLine(Text);
 
-		int option = 0;
-		bool first_line = true;
+        int option = 0;
+        bool first_line = true;
 
-		WriteColor(Text: "\t" + Options[option] + "\n", Background: ConsoleColor.White, Foreground: ConsoleColor.Black);
-		foreach(string initial_option in Options)
-		{
-			if(first_line){first_line=false;continue;}
-			Console.WriteLine("\t" + initial_option);
-		}
-		Console.CursorTop = 1;
+        WriteColor(Text: "\t" + Options[option] + "\n", Background: ConsoleColor.White, Foreground: ConsoleColor.Black);
+        foreach (string initial_option in Options)
+        {
+            if (first_line) { first_line = false; continue; }
+            Console.WriteLine("\t" + initial_option);
+        }
+        Console.CursorTop = 1;
 
-		while (true)
-		{
-			switch (Console.ReadKey(true).Key)
-			{
-				case ConsoleKey.DownArrow:
-					if (option + 1 < Options.Count())
-					{
-						Console.CursorLeft = 0;
-						Console.WriteLine("\t" + Options[option]);
+        while (true)
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.DownArrow:
+                    if (option + 1 < Options.Count())
+                    {
+                        Console.CursorLeft = 0;
+                        Console.WriteLine("\t" + Options[option]);
 
-						option++;
-						Console.CursorLeft = 0;
-						Console.CursorTop = option + 1;
-						WriteColor(Text: "\t" + Options[option], Background: ConsoleColor.White, Foreground: ConsoleColor.Black);
-					}
-					break;
-				case ConsoleKey.UpArrow:
-					if (option > 0)
-					{
-						Console.CursorLeft = 0;
-						Console.WriteLine("\t" + Options[option]);
+                        option++;
+                        Console.CursorLeft = 0;
+                        Console.CursorTop = option + 1;
+                        WriteColor(Text: "\t" + Options[option], Background: ConsoleColor.White, Foreground: ConsoleColor.Black);
+                    }
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (option > 0)
+                    {
+                        Console.CursorLeft = 0;
+                        Console.WriteLine("\t" + Options[option]);
 
-						option--;
-						Console.CursorLeft = 0;
-						Console.CursorTop = option + 1;
-						WriteColor(Text: "\t" + Options[option], Background: ConsoleColor.White, Foreground: ConsoleColor.Black);
-					}
-					break;
-				case ConsoleKey.Enter:
-					Console.Clear();
-					return option;
-			}
-		}
-	}
+                        option--;
+                        Console.CursorLeft = 0;
+                        Console.CursorTop = option + 1;
+                        WriteColor(Text: "\t" + Options[option], Background: ConsoleColor.White, Foreground: ConsoleColor.Black);
+                    }
+                    break;
+                case ConsoleKey.Enter:
+                    Console.Clear();
+                    return option;
+            }
+        }
+    }
 }
