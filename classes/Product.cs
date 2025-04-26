be65926f5f0285543138c185677f2fe186c1da57
spsDavidLastuvicka <lastuvickadavid@sps-karvina.cz>
2025-04-26 11:57:06 +0200

    Small type fix

--- classes/Product.cs
+++ classes/Product.cs
@@ -1,11 +1,11 @@
 public class Product
 {
     public static int Id { get; set; }
-    public static int Name { get; set; }
+    public static string? Name { get; set; }
     public static decimal Price { get; set; }
     public static int Quantity { get; set; }
 
-    public Product(int Id, int Name, decimal Price, int Quantity)
+    public Product(int Id, string? Name, decimal Price, int Quantity)
     {
         Product.Id = Id;
         Product.Name = Name;
